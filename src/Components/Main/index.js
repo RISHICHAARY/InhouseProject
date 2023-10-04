bf936fb374eabb0ea3bd65e2471e142e0b9a5ac5
RishiChaaryS <rs3715@srmist.edu.in>
2023-10-04 23:56:29 +0530

    first commit

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,6 +1,18 @@
+import {BrowserRouter as Router , Route , Routes} from 'react-router-dom';
+
+import LoginPage from '../LoginPage';
+import AdminPage from '../AdminPage';
+import UserPage from '../UserPage';
+
 function Main() {
   return (
-    <div></div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/adminPage' element={<AdminPage />} />
+        <Route path='/userPage' element={<UserPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './Components/Main';
 
+import "./Styles/Global.css";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Main/>
